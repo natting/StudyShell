06cde9a9473fcc3a396da13fbdb69e411b8e31d2
natting <39891002+natting@users.noreply.github.com>
2018-06-27 16:16:27 +0900

    Update test_00.sh

--- test_00.sh
+++ test_00.sh
@@ -16,10 +16,13 @@ echo a=$a
 echo b=$b
 echo c=$c
 
+# read test : 걍 read 변수하면 됨.
 echo -n "input sth:"    #-n = 개행
 read input  #read 함수로 input 변수에 입력
 echo "your input is $input"
 
+# expr test : 연산문을 위해서..
+# *는 다른데서 사용되므로 back slash 있어야함
 result=`expr $a + $b`
 echo "a+b = $result"
 result=`expr $a - $b`
@@ -29,11 +32,14 @@ echo "a*b = $result"
 result=`expr $b / $a`
 echo "b/a=$result"
 
+# while문 test : -lt, -gt, -ge, -le
 i=1
 while [ $i -lt 4 ]
 do
 	echo $i
 	i=$(($i+1))
 done
+
+# function test
 string_test
 string_test2
